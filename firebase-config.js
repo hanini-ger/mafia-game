c3a09ead4b54f6d0ea195bbe709e8545dc747f0c
hanini-ger <hanilee209@gmail.com>
2025-10-24 16:46:44 +0800

    Initial Firebase setup

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAICQgbjxiwmea-CPDUBGfCFveDfKqaD3s",
+  authDomain: "mafia-game-4b7be.firebaseapp.com",
+  projectId: "mafia-game-4b7be",
+  storageBucket: "mafia-game-4b7be.firebasestorage.app",
+  messagingSenderId: "867608731295",
+  appId: "1:867608731295:web:dd068f4f297121fa87ccaa",
+  measurementId: "G-DYDCMJ7GQC"
+};
